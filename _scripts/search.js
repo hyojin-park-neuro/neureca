71e795e13b12ba60bf2d087979ead6f117fcb3d5
Hyojin Park <h.park@bham.ac.uk>
2023-05-16 20:34:07 +0100

    Update search.js

--- _scripts/search.js
+++ _scripts/search.js
@@ -5,7 +5,7 @@
 */
 {
   // elements to filter
-  const elementSelector = ".card, .citation, .post-excerpt, .task-excerpt";
+  const elementSelector = ".card, .citation, .post-excerpt";
   // search box element
   const searchBoxSelector = ".search-box";
   // results info box element
